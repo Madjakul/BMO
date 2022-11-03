b0e03d73f949caaf90842471c996bfb53874ef0c
Francis <4865832-Madjakul@users.noreply.gitlab.com>
2022-11-03 17:11:51 +0100

    Changing main scirpts to take user argument, bash compatible

--- train_bmo.py
+++ train_bmo.py
@@ -1,5 +1,8 @@
 # train_bmo.py
 
+import logging
+import argparse
+
 import torch
 import torch.nn as nn
 from torch.optim import SGD
@@ -8,32 +11,38 @@ from bmo.utils.dataset import LMDataset
 from bmo.modules.word_embedding import CBOW
 
 
-EPOCH = 10
-VERVOSE = 2
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--corpus_path", type=str, default="./data/CDR_TrainingSet.txt"
+    )
+    parser.add_argument("--window_size", type=int, default=3)
+    parser.add_argument("--vocab_size", type=int, default=None)
+    parser.add_argument("--merging_size", type=int, default=None)
+    parser.add_argument("--device", choices=["cpu", "cuda"], default="cpu")
+    parser.add_argument("--embedding_size", type=int, default=300)
+    parser.add_argument("--epoch", type=int, default=10)
+    parser.add_argument("--update_rate", type=int, default=10)
+    parser.add_argument("--model_path", type=str, default="./tmp/bmo.torch")
+    args = parser.parse_args()
 
+    with open(args.corpus_path, "r", encoding="utf-8") as f:
+        corpus = f.read().splitlines()
+    dataset = LMDataset(
+        corpus=corpus, window_size=args.window_size, device=args.device,
+        vocab_size=args.vocab_size, merging_size=args.merging_size
+    )
+    dataset.build_vocab()
 
-def train_cbow(model: CBOW, train_data: LMDataset, valid_data: LMDataset=None):
-    """Trains the language model.
-
-    Parameters
-    ----------
-    model: ``bmo.modules.word_embedding.CBOW``
-        Language model.
-    train_data: ``bmo.utils.dataset.LMDataset``
-        Custom dataset to train the model.
-    valid_data: ``bmo.utils.dataset.LMDataset``
-        Custom dataset to validate the training.
-
-    Returns
-    -------
-    model: ``bmo.modules.word_embedding.CBOW``
-        Trained language model.
-    """
+    model = CBOW(
+        vocab_size=len(dataset.vocab), embedding_size=args.embedding_size,
+        context_size=args.window_size
+    )
     nll_loss = nn.NLLLoss() # Negative log-likelihood loss function
     optimizer = SGD(model.parameters(), lr=1e-3)
-    for epoch in range(EPOCH):
+    for epoch in range(args.epoch):
         total_loss = 0
-        for context, target in train_data:
+        for context, target in dataset:
             x = torch.LongTensor(context)
             y = torch.LongTensor([target])
             model.zero_grad()
@@ -42,28 +51,7 @@ def train_cbow(model: CBOW, train_data: LMDataset, valid_data: LMDataset=None):
             loss.backward()
             optimizer.step()
             total_loss += loss.data
-        if epoch % VERVOSE == 0:
+        if epoch % args.update_rate == 0:
             loss_avg = float(total_loss / len(train_data))
             print(f"{epoch}/{EPOCH} loss{loss_avg:.2f}")
-    return model
-
-def main():
-    corpus = [
-        "Hello nice to meet you.",
-        "Hi Francis, how is the meeting going?",
-        "How are you going to go to France?",
-        "Where is he from?",
-        "I don't know but he has gone mad.",
-        "Why is he going to meet the boss?"
-    ]
-    dataset = LMDataset(corpus=corpus, window_size=3, merging_size=20)
-    dataset.build_vocab()
-    vocab_size = len(dataset.vocab)
-    model = CBOW(vocab_size=vocab_size, embedding_size=128, context_size=3)
-    model = train_cbow(model, train_data=dataset)
-    print(model.get_embedding(dataset.vocab2ix["meet"]))
-
-
-if __name__=="__main__":
-    main()
 
